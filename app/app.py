a63e4a78661d209e6f1cc32516c43a8e0296f851
Vaibhav Pothireddy <vp373@njit.edu>
2021-04-17 12:40:15 -0400

    Task: Updated app, layout, and new

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+app.config['MYSQL_DATABASE_DB'] = 'homesData'
 mysql.init_app(app)
 
 
